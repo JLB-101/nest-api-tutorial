530a61c6a2a7e3ddec51533f54bdbe962227bc03
Julião LB <juliaolangassitela@gmail.com>
2024-03-04 08:32:13 +0200

    add bookmark and user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.modules';// importando o modulo de autenticacao
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 // aqui podemos importar todos modulos(modulo de autenticacao, etc)
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UserModule, BookmarkModule],
  
 })
 export class AppModule {}
