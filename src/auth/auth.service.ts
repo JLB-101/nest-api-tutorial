567b44f4d68f054b2a7e29632f7ad4d00f196ca4
Julião LB <juliaolangassitela@gmail.com>
2024-03-13 22:32:48 +0200

    fixing error: by exporting prismaService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
 import { Injectable, Post } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable({})// para servicos
 export class AuthService {
+    constructor(private prisma: PrismaService){
+        
+    }
     signup() {
         return  {msg: 'I have signed up'};
     }
