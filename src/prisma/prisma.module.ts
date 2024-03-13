683799241fdefa94a7081ae0ff9a0b386c4241d9
Julião LB <juliaolangassitela@gmail.com>
2024-03-13 22:37:37 +0200

    turning prismaServic at mudule global

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,5 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+
+//make this global to acessed anywhere - to not repeat or create many module
+@Global()//now the prismaService is avaliable to all de module
 
 @Module({
   providers: [PrismaService],
